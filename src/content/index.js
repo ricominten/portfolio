a6d96661fc768005585beaf41f5cc46130b84997
Rico Minten <ricominten@gmail.com>
2021-07-11 18:57:48 +1000

    feat: project

--- src/content/index.js
+++ src/content/index.js
@@ -1,9 +1,11 @@
 import weekend from './weekend';
 import helloWorld from './hello-wold';
+import jsConsole from './js-console';
 
 const content = [
     weekend,
     helloWorld,
+    jsConsole,
 ];
 
 export default content;
---
+++ src/content/js-console/index.js
@@ -0,0 +1,16 @@
+import img from './js-console.png';
+
+const article =  {
+    title: 'JS Console App',
+    image: img,
+    alt: 'Screenshot of JS Console Electron app',
+    about: `Your browsers console, but in an app. JS Console App is an updated standalone version of the open source 
+        <a href="https://jsconsole.com/" target="_blank" rel="noreferrer nooperner">https://jsconsole.com/</a>`,
+    tech: `<i>JS Console App</i> is a project where to port <a href="https://github.com/remy/jsconsole" target="_blank" 
+        rel="noreferrer noopener">jsconsole</a> into a standalone app using Electron. In the process of 
+        porting the styling was updated using Emotion CSS and a few issues around es6 syntax were fixed.`,
+    link: 'https://github.com/ricominten/jsconsole/releases',
+    github: 'https://github.com/ricominten/jsconsole',
+};
+
+export default article;
